15fdb2b3437d80f06c21c00870445bb09dc32dec
t2wole <t2wole@gmail.com>
2022-04-02 00:40:29 +0900

    second

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-alert("hello");
+const name = "jay",
+    age = 25,
+    gender = "male";
+
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};  // 오류 고치는 ts만의 규칙
